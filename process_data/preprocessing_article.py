fd2a729d6f6764b471cf3d97114f6f67f7c3191b
kirillsst <jewaor@gmail.com>
2025-10-23 11:16:02 +0200

    fix: stopwords deleted

--- process_data/preprocessing_article.py
+++ process_data/preprocessing_article.py
@@ -1,38 +1,23 @@
 from article_processor import ArticleProcessor
 import re
-from nltk.corpus import stopwords
-import nltk
-
-nltk.download("stopwords", quiet=True)
 
 class PreprocessingArticle(ArticleProcessor):
-    def __init__(self, text, label=None, lemmatize=True, remove_stopwords=True, nlp_model=None):
+    def __init__(self, text, label=None, lemmatize=True, nlp_model=None):
         super().__init__(text, label)
         self.lemmatize = lemmatize
-        self.remove_stopwords = remove_stopwords
-        self.stop_words = set(stopwords.words("english"))
-        self.nlp = nlp_model 
+        self.nlp = nlp_model
 
     def clean_text(self):
-        text = self.text.lower()
+        text = self.text.lower() # mise en minuscules
         text = re.sub(r"http\S+", "", text)
         text = re.sub(r"<.*?>", "", text)
         text = re.sub(r"[^a-zA-Z0-9\s]", " ", text)
         text = re.sub(r"\s+", " ", text).strip()
-
-        if self.remove_stopwords and not self.lemmatize:
-            tokens = [word for word in text.split() if word not in self.stop_words]
-            text = " ".join(tokens)
-
         self.text = text
         return self.text
 
     def lemmatize_text(self):
         if self.lemmatize and self.nlp:
             doc = self.nlp(self.text)
-            
-            self.text = " ".join([
-                token.lemma_ for token in doc
-                if not (self.remove_stopwords and token.is_stop)
-            ])
+            self.text = " ".join([token.lemma_ for token in doc])
         return self.text
