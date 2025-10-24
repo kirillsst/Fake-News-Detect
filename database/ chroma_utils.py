1d754e01916d6bfdb3355424850c3f12ce8600e9
Marwa <marwa-aboulkasim@hotmail.fr>
2025-10-24 11:18:59 +0200

    maj

--- database/ chroma_utils.py
+++ database/ chroma_utils.py
@@ -1,16 +1,16 @@
-import re
-import numpy as np
+# import re
+# import numpy as np
 
-def preprocess(text):
-    text = re.sub(r'<[^>]+>', '', str(text))
-    text = re.sub(r'[^a-zA-ZÀ-ÿ0-9\s]', '', text)
-    return text.lower().strip()
+# def preprocess(text):
+#     text = re.sub(r'<[^>]+>', '', str(text))
+#     text = re.sub(r'[^a-zA-ZÀ-ÿ0-9\s]', '', text)
+#     return text.lower().strip()
 
-def chunk_text(text, chunk_size=300):
-    words = text.split()
-    return [' '.join(words[i:i+chunk_size]) for i in range(0, len(words), chunk_size)]
+# def chunk_text(text, chunk_size=300):
+#     words = text.split()
+#     return [' '.join(words[i:i+chunk_size]) for i in range(0, len(words), chunk_size)]
 
-def normalize_vector(v):
-    v = np.array(v)
-    norm = np.linalg.norm(v)
-    return (v / norm).tolist() if norm != 0 else v.tolist()
+# def normalize_vector(v):
+#     v = np.array(v)
+#     norm = np.linalg.norm(v)
+#     return (v / norm).tolist() if norm != 0 else v.tolist()
--- database/chroma_insert.py
+++ database/chroma_insert.py
@@ -1,20 +1,20 @@
-# from ollama._client import Client as OllamaClient
-# from .chroma_setup import get_chroma_collection
-# from .chroma_utils import normalize_vector
+from ollama._client import Client as OllamaClient
+from .chroma_setup import get_chroma_collection
+from .chroma_utils import normalize_vector
 
-# ollama_client = OllamaClient()
+ollama_client = OllamaClient()
 
-# def insert_chunk(chunk_id, text, metadata, collection):
-#     try:
-#         embedding_response = ollama_client.embeddings(model="all-minilm", prompt=text)
-#         embedding = normalize_vector(embedding_response["embedding"])
+def insert_chunk(chunk_id, text, metadata, collection):
+    try:
+        embedding_response = ollama_client.embeddings(model="all-minilm", prompt=text)
+        embedding = normalize_vector(embedding_response["embedding"])
 
-#         collection.add(
-#             ids=[chunk_id],
-#             documents=[text],
-#             embeddings=[embedding],
-#             metadatas=[metadata]
-#         )
-#         print(f" Chunk {chunk_id} ajouté.")
-#     except Exception as e:
-#         print(f" Erreur avec le chunk {chunk_id} : {e}")
+        collection.add(
+            ids=[chunk_id],
+            documents=[text],
+            embeddings=[embedding],
+            metadatas=[metadata]
+        )
+        print(f" Chunk {chunk_id} ajouté.")
+    except Exception as e:
+        print(f" Erreur avec le chunk {chunk_id} : {e}")
