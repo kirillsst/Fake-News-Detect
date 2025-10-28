c56493cd3d113253c2efc59e962b4f364ab068a2
Chaima425 <chaimaabdraouf@gmail.com>
2025-10-28 12:05:02 +0100

    notebook :( Implémentation complète du système RAG avec  justification du verdict TRUE/FAKE) + mise a  jour fichier  rag_pipleine.py

--- database/embeddings.py
+++ database/embeddings.py
@@ -1,10 +1,10 @@
 import ollama
 import numpy as np
 
+
 def chunk_text(text, max_words=130):
     words = text.split()
     return [" ".join(words[i:i+max_words]) for i in range(0, len(words), max_words)]
-
 def get_embedding(text, model="all-minilm"):
     """
     Récupère l'embeddage du texte. 
