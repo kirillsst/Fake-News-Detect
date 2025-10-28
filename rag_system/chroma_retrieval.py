6ebd6904ff4da3a13fdc43920a43a23faf910eaf
kirillsst <jewaor@gmail.com>
2025-10-28 14:48:15 +0100

    add: label in context_chunks

--- rag_system/chroma_retrieval.py
+++ rag_system/chroma_retrieval.py
@@ -1,5 +1,7 @@
 # Recherche de documents similaires dans ChromaDB
-
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from chromadb import PersistentClient
 from database.chroma_utils import get_embedding
 
@@ -14,5 +16,9 @@ def get_context_from_chroma(user_text: str, n_results: int = 5) -> str:
     client = PersistentClient(path=CHROMA_DB_PATH)
     collection = client.get_collection(COLLECTION_NAME)
     results = collection.query(query_embeddings=[query_embedding], n_results=n_results)
-    context_chunks = results["documents"][0]
-    return " ".join(context_chunks)
+    context_chunks = []
+    for doc, metadata in zip(results["documents"][0], results["metadatas"][0]):
+        context_chunks.append(f"[{metadata['label'].upper()}] {doc}")
+
+    context_text = " ".join(context_chunks)
+    return context_text
